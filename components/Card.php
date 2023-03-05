9ec52cb86a9bba2d4c9b5831851b8e046a21ef61
DipakDai <dipakkumal673@gmail.com>
2023-03-06 00:01:12 +0545

    frontend complete

---
+++ components/Card.php
@@ -0,0 +1,36 @@
+<?php
+
+function Card($img,$name,$description,$price,$realPrice) {
+    $element = "
+    <div class='col-md-3 col 6 my-3 md-0'>
+    <div class='card shadow'>
+        <div class='text-center'>
+            <img src=\"$img\" alt='' class='img-fluid'>
+        </div>
+        <div class='card body'>
+            <h5 class='card-title'>$name</h5>
+            <h6>
+                <i class='fas fa-star'></i>
+                <i class='fas fa-star'></i>
+                <i class='fas fa-star'></i>
+                <i class='fas fa-star'></i>
+                <i class='fas fa-star-half'></i>
+            </h6>
+            <p class='card-text'>$description</p>
+            <h5>
+                <small><s class='text-secondary'>$$realPrice</s></small>
+                <span class='price'>$$price</span>
+            </h5>
+            <button type='submit' class='btn btn-warning my-3' name='add'>Add to Cart <i class='fas fa-shopping-cart'></i></button>
+        </div>
+    </div>
+</div>
+    ";
+
+    echo $element;
+}
+
+?>
+
+
+<img src="../images/1.jpg" alt="">
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+
+include './components/Card.php';
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Shopping Cart</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row text-center py-5">
+           <?php 
+                Card("./components/images/1.jpg","Product1","Sony ZX Series Wired On-Ear Headphones, Black MDR-ZX110",400,3000);
+                Card("./components/images/2.jpg","Product3","Sony ZX Series Wired On-Ear Headphones, Black MDR-ZX110",400,3000);
+                Card("./components/images/3.jpg","Product4","Sony ZX Series Wired On-Ear Headphones, Black MDR-ZX110",400,3000);
+                Card("./components/images/3.jpg","Product5","Sony ZX Series Wired On-Ear Headphones, Black MDR-ZX110",400,3000);
+           ?>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
